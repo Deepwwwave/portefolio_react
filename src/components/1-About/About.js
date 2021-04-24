e7a4ea98e6c524c3dd50bf323f06e00157abe95c
deepwwwave <deepwwwave@gmail.com>
2021-04-24 23:39:31 +0200

    evrything ok

--- src/components/1-About/About.js
+++ src/components/1-About/About.js
@@ -17,24 +17,24 @@ export default class About extends Component {
     }
 
     render() {
-        return(  <section class="resume-section p-3 p-lg-5 d-flex align-items-center">
-        <div class="w-100">{this.state.first}
-          <h1 class="mb-0">{this.state.last}
-        <span class="text-primary"></span>
+        return(  <section className="resume-section p-3 p-lg-5 d-flex align-items-center">
+        <div className="w-100">
+          <h1 className="mb-0">{this.state.first}
+        <span className="text-primary">{this.state.last}</span>
           </h1>
-          <div class="subheading mb-5">{this.state.address}
+          <div className="subheading mb-5">
     <a href="mailto:name@email.com">{this.state.email}</a>
           </div>
-          <p class="lead mb-5"></p>
-          <div class="social-icons">
+          <p className="lead mb-5">{this.state.bio}</p>
+          <div className="social-icons">
             <a href={this.state.social.linkedin}>
-              <i class="fab fa-linkedin-in"></i>
+              <i className="fab fa-linkedin-in"></i>
             </a>
             <a href={this.state.social.github}>
-              <i class="fab fa-github"></i>
+              <i className="fab fa-github"></i>
             </a>
             <a href={this.state.social.twitter}>
-              <i class="fab fa-twitter"></i>
+              <i className="fab fa-twitter"></i>
             </a>
           </div>
         </div>
